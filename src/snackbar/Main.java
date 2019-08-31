c18aa0eec7a925a178cdc2ef75962cfa361297ea
Patrick Gordon <patrickgordon42@yahoo.com>
2019-08-30 19:05:22 -0500

    instantiated Customers and Vending Machines

---
+++ src/snackbar/Main.java
@@ -0,0 +1,16 @@
+package snackbar;
+
+public class Main {
+
+    public static void workWithData(){
+        Customer c1 = new Customer("Jane", 45.25);
+        Customer c2 = new Customer("Bob", 33.14);
+
+        VendingMachine vm1 = new VendingMachine("Food");
+        VendingMachine vm2 = new VendingMachine("Drink");
+        VendingMachine vm3 = new VendingMachine("office");
+
+        Snack s1 = new Snack()
+
+    }
+}
